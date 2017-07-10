1ef89fadff6bcb84313a27262fa93ce0ccddada6
PankajMehar <pankaj02mehar@gmail.com>
2017-07-10 10:55:23 +0000

    Done

--- build.py
+++ build.py
@@ -10,11 +10,5 @@ labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 
 def solution():
-    """
-    Enter your code here
-    Function should not return anything. Test cases will pass any how.
-    This exercise it to provide you information for info function.
-    """
-
-
-solution()
+    df = DataFrame(exam_data,label = labels)
+    return df.info()
